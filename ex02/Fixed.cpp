2a47d72216d67ec8725f6bf2203808644555679b
Kathryn Sansom <ksansom@c3a8c5.42berlin.de>
2024-05-03 14:19:28 +0200

    adding ex02

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,51 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _fixedPoint(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &func) {
+	std::cout << "Copy constructor called" << std::endl;
+	this->setRawBits(func.getRawBits());
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &func2) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->setRawBits(func2.getRawBits());
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const {
+	return (_fixedPoint);
+}
+
+void Fixed::setRawBits( int const raw ) {
+	this->_fixedPoint = raw;
+}
+
+Fixed::Fixed(const int integer) {
+	std::cout << "Int constructor called" << std::endl;
+	this->_fixedPoint = (integer << Fixed::_bits);
+}
+
+Fixed::Fixed(const float floating) {
+	std::cout << "Float constructor called" << std::endl;
+	this->_fixedPoint = roundf(floating * (1 << this->_bits));
+}
+
+float	Fixed::toFloat( void ) const {
+	return ((float)this->_fixedPoint / (float)(1 << this->_bits));
+}
+
+int	Fixed::toInt( void ) const {
+	return (this->_fixedPoint >> this->_bits);
+}
+
+std::ostream	&operator<<(std::ostream &str, Fixed const &func) {
+	str << func.toFloat();
+	return str;
+}
---
+++ ex02/Fixed.hpp
@@ -0,0 +1,28 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+	private:
+		int					_fixedPoint;
+		static const int	_bits = 8;
+	public:
+		Fixed();
+		Fixed(const Fixed &func);
+		~Fixed();
+		Fixed &operator=(const Fixed &func2);
+		
+		int 	getRawBits( void ) const;
+		void	setRawBits( int const raw );
+
+		Fixed(const int integer);
+		Fixed(const float floating);
+
+		float 	toFloat( void ) const;
+		int 	toInt( void ) const;
+};
+
+std::ostream& operator<<(std::ostream& str, const Fixed& func3);
+#endif
